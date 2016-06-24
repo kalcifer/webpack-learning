cfd11d98ad1ae6f42a8aae47d3064e9466592a07
Pavithra K <pksjce@gmail.com>
2016-06-24 20:04:36 +0530

    More requires and --watch

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 This is me learning webpack and its features. At each commit, I will learn a new feature of webpack and post my learnings in the commit"
 
 001:npm install webpack --save-dev && webpack index.js dist/build.js
-
+002:a little complex require and watch option to webpack. Note: shows only changed file in log while recompiling during watch
--- dosomething.js
+++ dosomething.js
@@ -1,3 +1,8 @@
 module.exports = function dosomething() {
-	document.getElementById('root').innerHTML = "Look! I am learning webpack.";
+	document.getElementById('root').innerHTML = "<button>I will fetch a remote thing.</button>";
+	var button = document.getElementsByTagName('button')[0];
+	button.addEventListener('click', function(){
+		var remotething = require('./remotething.js');
+		remotething();
+	})
 };
