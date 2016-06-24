c11ec73f5ab910351b8377ac9e7c4dac98d5b080
Pavithra K <pksjce@gmail.com>
2016-06-24 20:15:57 +0530

    Simple code splitting example.

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ This is me learning webpack and its features. At each commit, I will learn a new
 
 001:npm install webpack --save-dev && webpack index.js dist/build.js
 002:a little complex require and watch option to webpack. Note: shows only changed file in log while recompiling during watch
+003:simple example for code splitting. Using require.ensure()
--- dosomething.js
+++ dosomething.js
@@ -2,7 +2,9 @@ module.exports = function dosomething() {
 	document.getElementById('root').innerHTML = "<button>I will fetch a remote thing.</button>";
 	var button = document.getElementsByTagName('button')[0];
 	button.addEventListener('click', function(){
-		var remotething = require('./remotething.js');
-		remotething();
+		require.ensure('./remotething.js', function(){
+			var remotething = require('./remotething.js');
+			remotething();
+		});
 	})
 };
